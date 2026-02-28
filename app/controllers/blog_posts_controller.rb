01b600f22408587b06be84ed98e0401a4a82f00e
Usama441 <usamaarshad441@gmail.com>
2026-03-01 02:17:36 +0500

    Edit & Update the blog post video 9

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -20,8 +20,27 @@ class BlogPostsController < ApplicationController
     end
   end
 
+ def edit
+    @blog_post = BlogPost.find(params[:id])
+  end 
+
+  def update
+    @blog_post = BlogPost.find(params[:id])
+    if @blog_post.update(blog_post_params)
+      redirect_to blog_post_path(@blog_post)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @blog_post = BlogPost.find(params[:id])
+    @blog_post.destroy!
+    redirect_to blog_posts_path, status: :see_other
+  end
+
   def blog_post_params
     params.require(:blog_post).permit(:title, :body)
   end
-  
+
 end
