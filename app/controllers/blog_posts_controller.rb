68c657b3913064cc2594e8d34fee8f2f9debb2b2
Usama441 <usamaarshad441@gmail.com>
2026-03-01 03:30:53 +0500

    Adding Blog Post Destroy Action Video 10

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,11 +1,11 @@
 class BlogPostsController < ApplicationController
-   
+   before_action :set_blog_post, except: [:index, :new, :create]
   def index
     @blog_posts = BlogPost.all
   end
 
   def show
-    @blog_posts = BlogPost.find(params[:id])
+  
   end
   def new
     @blog_post = BlogPost.new
@@ -21,11 +21,11 @@ class BlogPostsController < ApplicationController
   end
 
  def edit
-    @blog_post = BlogPost.find(params[:id])
+  
   end 
 
   def update
-    @blog_post = BlogPost.find(params[:id])
+
     if @blog_post.update(blog_post_params)
       redirect_to blog_post_path(@blog_post)
     else
@@ -34,13 +34,21 @@ class BlogPostsController < ApplicationController
   end
 
   def destroy
-    @blog_post = BlogPost.find(params[:id])
-    @blog_post.destroy!
-    redirect_to blog_posts_path, status: :see_other
+
+    @blog_post.destroy
+    redirect_to root_path
   end
+
+  private 
 
   def blog_post_params
     params.require(:blog_post).permit(:title, :body)
+  end
+
+  def set_blog_post
+    @blog_post = BlogPost.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      redirect_to root_path, alert: "Blog post not found" 
   end
 
 end
