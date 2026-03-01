f58b15f24f395e96f59966d09d040e7e97a472de
Usama441 <usamaarshad441@gmail.com>
2026-03-01 21:03:20 +0500

    Adding css and devise views video:12

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+user = User.where(email: "admin@example.com").find_or_initialize_by(email: "admin@example.com")
+user.update!(password: "password", password_confirmation: "password")
