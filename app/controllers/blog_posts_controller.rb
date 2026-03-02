7537cdd9390d2b51d9549872b3d06a9b5d87cf41
Usama441 <usamaarshad441@gmail.com>
2026-03-02 20:12:16 +0500

    Adding Scheduled Blog post

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -2,7 +2,7 @@ class BlogPostsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
    before_action :set_blog_post, except: [:index, :new, :create]
   def index
-    @blog_posts = BlogPost.all
+    @blog_posts = user_signed_in? ? BlogPost.sorted : BlogPost.published.sorted
   end
 
   def show
@@ -43,12 +43,17 @@ class BlogPostsController < ApplicationController
   private 
 
   def blog_post_params
-    params.require(:blog_post).permit(:title, :body)
+    params.require(:blog_post).permit(:title, :body, :published_at)
   end
 
   def set_blog_post
-    @blog_post = BlogPost.find(params[:id])
-    rescue ActiveRecord::RecordNotFound
+
+    if user_signed_in?
+      @blog_post = BlogPost.find(params[:id])
+    else
+      @blog_post = BlogPost.published.find(params[:id])
+    end  
+  rescue ActiveRecord::RecordNotFound
       redirect_to root_path, alert: "Blog post not found" 
   end
 
